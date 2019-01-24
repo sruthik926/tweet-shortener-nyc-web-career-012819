a459e3b768ffc6fee9540cb18cd494b8caec9324
Sruthi Krishna <sruthik926@gmail.com>
2019-01-24 16:44:23 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,10 +44,9 @@ def selective_tweet_shortener(tweet)
   else 
      return tweet  
   end
-  
 end
 
-def shortened_tweet_truncator
-  
+def shortened_tweet_truncator(tweet)
+  tweet[0...140]
   
 end
